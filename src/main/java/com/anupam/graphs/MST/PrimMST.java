b8d4ac679cd5b5b0f1ef324fbaca84e3d615d074
bk-anupam <bk_anupam@outlook.com>
2020-04-29 10:42:05 +0530

    Refactored code to remove unnecessary loop. Added comments

--- src/main/java/com/anupam/graphs/MST/PrimMST.java
+++ src/main/java/com/anupam/graphs/MST/PrimMST.java
@@ -49,28 +49,44 @@ public class PrimMST {
 
     private void visit(Node node) throws Exception{
         visitedVertices.add(node.getVertex());
-        for(int visitedVertex: visitedVertices){
+        int visitedVertex = node.getVertex();
+        //for(int visitedVertex: visitedVertices){
             for(WeightedEdge edge: ewg.getEdges(visitedVertex)){
                 if(isCrossingEdge(edge)){
                     int v = edge.other(visitedVertex);
                     if( edge.weight() < distTo[v]) {
                         distTo[v] = edge.weight();
                         edgeTo[v] = edge;
+                        // Since the edge the from visitedVertex (in X) to v (in V-X) where X is the set of visited vertices
+                        // and V is the set of all vertices, being a crossing edge and its weight is less than the
+                        // weight of the current edge (distTo[v]) that connects v to the MST, hence we will update
+                        // the minPQ and mark visitedVertex as the parent of v and edge.weight as the weight of v.
+                        // This is done by doing a remove and an add since PQ doesn't provide and update function
+                        // Both remove and add have a time complexity of O(logn)
                         vertexMinWeightPQ.remove(node);
                         vertexMinWeightPQ.add(new Node(v, visitedVertex, edge.weight()));
                     }
+                 // if the edge is not a crossing edge, surely both the vertices of the edge must have been visited
+                 // already and the edge has been identified as part of MST (being a greedy algorithm this consideration
+                 // is irrevocable ) and hence this edge can be removed from the min PQ.
+                }else{
+                    vertexMinWeightPQ.remove(node);
                 }
             }
-        }
+        //}
     }
 
+    // By cut property of a graph, if one of edge vertices is in the visited set and the other not, the edge
+    // is a crossing edge.
     private boolean isCrossingEdge(WeightedEdge we) throws Exception{
         int u = we.either();
         int v = we.other(u);
-        if(visitedVertices.contains(u) && visitedVertices.contains(v))
-            return false;
-        else
+        if(visitedVertices.contains(u) && !visitedVertices.contains(v))
             return true;
+        else if(visitedVertices.contains(v) && !visitedVertices.contains(u))
+            return true;
+        else
+            return false;
     }
 
     public WeightedEdge[] getMst(){
